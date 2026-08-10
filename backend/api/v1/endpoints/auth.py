154ffcbf5bcb2f7072a31fae110905d6b78c3d5f
Anjel Cat <142579884+anjelisa01@users.noreply.github.com>
2026-08-10 06:09:00 +0000

    cleaned auth service and endpoint

--- backend/api/v1/endpoints/auth.py
+++ backend/api/v1/endpoints/auth.py
@@ -1,21 +1,9 @@
 from fastapi import APIRouter,Depends
-from sqlalchemy.orm import Session
 from fastapi.security import OAuth2PasswordRequestForm
 
-#pydantic schemas
 from schemas.auth import UserLogin                                                      
-
-#exception handler:fastapi (app level)
-from fastapi import HTTPException
-
-#exception
-from core.exceptions import AuthFailedCredential
-
-# services
 from service.auth_service import AuthService
-#dependencies
 from dependencies.services import get_auth_service
-
 
 router=APIRouter(tags=["auth"])
 
@@ -25,10 +13,4 @@ def auth_login(form_data: OAuth2PasswordRequestForm = Depends(),service:AuthServ
         email=form_data.username,  # Swagger username = your email
         password=form_data.password
     )
-    try: 
-        return service.login(payload)
-    except AuthFailedCredential:
-        raise HTTPException(
-            status_code=401,
-            detail="Failed credential"
-        )
+    return service.login(payload)
--- backend/service/auth_service.py
+++ backend/service/auth_service.py
@@ -1,23 +1,24 @@
 #import
 from sqlalchemy.orm import Session
-from sqlalchemy import select
 
-#schema
 from schemas.auth import UserLogin
 
-#security utils
 from core.security import get_user_by_email,verify_password,create_access_token
 from core.logger import logger
+from core.exceptions import AuthFailedCredential
 
 class AuthService:
     def __init__(self,db: Session):
         self.db = db
     def login(self,payload:UserLogin):
         existed_user=get_user_by_email(self.db,payload.email)
-        #expected warning
+        if not existed_user or not verify_password(self.db,payload.password,existed_user.hashed_password):
+            raise AuthFailedCredential()
+
         try:
             token=create_access_token({"user_id":str(existed_user.id)})
         except Exception:
             raise
+
         logger.info("User created token. user_id=%s", existed_user.id)
         return {"access_token":token,"token-type":"bearer"}
